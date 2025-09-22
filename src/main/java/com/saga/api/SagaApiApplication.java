420e3b8b6bfe965ac185dc9dd8d09a5e0b6c9707
alfredoerevollo@gmail.com <Alfredo@BCSC59TC1046973.fs.net>
2025-09-22 16:39:50 -0500

    Initial commit

---
+++ src/main/java/com/saga/api/SagaApiApplication.java
@@ -0,0 +1,13 @@
+package com.saga.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaApiApplication.class, args);
+    }
+
+}
