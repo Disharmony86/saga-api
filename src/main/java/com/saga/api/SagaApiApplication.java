1919c8e2f813ae26bf2d1c0455ea971316dbb014
alfredoerevollo@gmail.com <Alfredo@BCSC59TC1046973.fs.net>
2025-09-23 15:54:56 -0500

    Agregué una clase controller y se probo un metodo que es un endpoint con GetMapping /api/health

--- src/main/java/com/saga/api/SagaApiApplication.java
+++ src/main/java/com/saga/api/SagaApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SagaApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SagaApiApplication.class, args);
     }
 
